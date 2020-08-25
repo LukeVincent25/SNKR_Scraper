edefd1fcd58943d08d1685b34170e5ef792ffbfb
LukeVincent25 <lavincent15@gmail.com>
2020-08-25 00:18:21 -0500

    Finished Rough

--- snkr_webiste_buy.py
+++ snkr_webiste_buy.py
@@ -1,63 +1,69 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 options = webdriver.ChromeOptions()
-prefs = {"profile.managed_default_content_setting.images": 2}
+options.add_argument("start-maximized")
+options.add_argument('disable-infobars')
+prefs = {"profile.managed_default_content_settings.images": 2}
 options.add_experimental_option("prefs", prefs)
+
+
 driver = webdriver.Chrome(chrome_options=options)
 
 CART = False
-
+SHOE_AMOUNT = 1
+SIZE=0
+retries = 0
 
 while True:
     if CART == False:
-        driver.get("https://www.nike.com/launch/t/adapt-bb-2-0-tie-dye")
+        #driver.get("https://www.nike.com/launch/t/adapt-bb-2-0-tie-dye")
+        driver.get("https://www.nike.com/launch/t/sb-dunk-low-medicom-be-rbrick")
+        #driver.get("https://www.nike.com/launch/t/blazer-mid-77-grey-fog")
 
         # Click Button
 
 
         # click size
         print("Selecting Size")
-        size_path = "/html/body/div[2]/div/div/div[1]/div/div[3]/div[2]/div/section/div[2]/aside/div/div[2]/div/div[2]/ul/li[7]/button"
-        size_button = driver.find_elements_by_xpath(size_path)
-        size_button[0].click()
-
+        size_path = '//*[@id="root"]/div/div/div[1]/div/div[3]/div[2]/div/section/div[2]/aside/div/div[2]/div/div[2]/ul/li[9]/button'
+        try:
+            WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, size_path))).click()
+        except:
+            retries = retries + 1
+            print("retries: ")
+            print(retries)
+            continue
         # add to cart
         print("Adding to cart")
-        cart_path = "/html/body/div[2]/div/div/div[1]/div/div[3]/div[2]/div/section/div[2]/aside/div/div[2]/div/div[2]/div/button"
-        cart_button = driver.find_elements_by_xpath(cart_path)
-        cart_button[0].click()
+        cart_path = '//*[@id="root"]/div/div/div[1]/div/div[3]/div[2]/div/section/div[2]/aside/div/div[2]/div/div[2]/div/button'
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, cart_path))).click()
+        time.sleep(3)
 
     # Go to Checkout
     print("Going to Checkout")
     driver.get("https://www.nike.com/us/en/checkout")
 
-    print("sleeping 5 seconds")
-    time.sleep(5)
-
-    # Fill in information
     first_name = "Alex"
     first_name_path = '//*[@id="firstName"]'
-    first_name_box = driver.find_elements_by_xpath(first_name_path)
     try:
-        first_name_box[0].send_keys(first_name)
+        WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, first_name_path))).send_keys(first_name)
     except:
-        print("Nothing in cart. Retrying.")
         continue
-
-    CART = True
 
     last_name = "Chance"
     last_name_path = '//*[@id="lastName"]'
     last_name_box = driver.find_elements_by_xpath(last_name_path)
     last_name_box[0].send_keys(last_name)
 
-    address = "4504 W Alberta ST"
+    address = "2473 N East Oaks Dr Fayetteville AR 72701"
     address_path = '//*[@id="search-address-input"]'
     address_box = driver.find_elements_by_xpath(address_path)
     address_box[0].send_keys(address)
-    address_box[0].send_keys(Keys.RETURN)
 
     email = "email@gmail.com"
     email_path = '//*[@id="email"]'
@@ -71,68 +77,68 @@ while True:
 
     address_box[0].send_keys(Keys.RETURN)
 
-
     # Save and Continue
     print("Save and Continue")
     save_and_continue_path = '//*[@id="shipping"]/div/div[2]/form/div/div/div/div[2]/button'
-    save_and_continue_button = driver.find_elements_by_xpath(save_and_continue_path)
-    try:
-        save_and_continue_button[0].click()
-    except:
-        print("failed to click continue button. Retrying...")
-        continue
-
-    print("sleeping")
-    time.sleep(5)
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, save_and_continue_path))).click()
 
     # Continue to Payment
     print("Continue to Payment")
     ctp_path = '//*[@id="shipping"]/div/div[3]/div/button'
     try:
-        ctp_button = driver.find_element_by_xpath(ctp_path)
+        WebDriverWait(driver, 4).until(EC.element_to_be_clickable((By.XPATH, ctp_path))).click()
     except:
-        print("Couldn't find the continue to payment button. Retrying...")
-        continue
-
-    try:
-        ctp_button[0].click()
-    except:
-        try:
-            ctp_button.click()
-        except:
-            print("couldn't click ctp button")
-            break
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, save_and_continue_path))).click()
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, ctp_path))).click()
     
-    print("sleeping")
-    time.sleep(5)
 
     # Fill CC Info
     print("Filling in Credit Card information")
-    cc_num = '0000111122223333'
-    cc_path = '/html/body/form/div/div[1]/input'
-    cc_box = driver.find_elements_by_id("creditCardNumber")
-    cc_box[0].send_keys(cc_num)
-    try:
-        cc_box[0].send_keys(cc_num)
-    except:
-        try:
-            cc_box.send_keys(cc_num)
-        except:
-            print("Failed to enter CC")
-            break
+    cc_num = '4744 8300 0844 3270'
+    cc_path = '//*[@id="creditCardNumber"]'
+    cc_iframe = '//*[@id="payment"]/div/div[1]/div[2]/div[4]/div/div[1]/div[2]/iframe'
+
+    WebDriverWait(driver, 20).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,cc_iframe)))
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, cc_path))).send_keys('47448300')
+    time.sleep(.5)
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, cc_path))).send_keys('08')
+    time.sleep(.5)
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, cc_path))).send_keys('44')
+    time.sleep(.5)
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, cc_path))).send_keys('3')
+    time.sleep(.5)
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, cc_path))).send_keys('2')
+    time.sleep(.5)
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, cc_path))).send_keys('70')
+
 
     # Fill CC Expiration
-    cc_exp = '10/25'
+    cc_exp = '1021'
     cc_exp_path = '//*[@id="expirationDate"]'
-    cc_exp_box = driver.find_elements_by_xpath(cc_exp_path)
-    cc_exp_box[0].send_keys(cc_exp)
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, cc_exp_path))).send_keys(cc_exp)
 
     # Security Code
-    sc = '123'
-    sc_path = '//*[@id="expirationDate"]'
-    sc_box = driver.find_element_by_xpath(sc_path)
-    sc_box[0].send_keys(sc)
+    sc = '981'
+    sc_path = '//*[@id="cvNumber"]'
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, sc_path))).send_keys(sc)
 
+    time.sleep(.5)
+    # Switching to Parent Frame
+    driver.switch_to.default_content
+    time.sleep(.5)
+    # Review Order
+    review_path = '//*[@id="payment"]/div/div[1]/div[2]/div[5]/button'
+    try:
+        WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, review_path))).click()
+    except:
+        driver.switch_to.default_content
+        WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, review_path))).click()
+
+
+    # Buy
+    buy_path = '//*[@id="orderreview"]/div/div/div/div/section[2]/div/button'
+    #WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, buy_path))).click()
+    print("SHOE BOUGHT")
     print("Program Terminated")
     break
 
