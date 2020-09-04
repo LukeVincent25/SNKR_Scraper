69440b4ea21177d1c5a1f9a74045b1d774c875b0
LukeVincent25 <lavincent15@gmail.com>
2020-09-03 22:35:11 -0500

    final

--- new_drop_snkr.py
+++ new_drop_snkr.py
@@ -5,45 +5,84 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+import tkinter
+
 options = webdriver.ChromeOptions()
 options.add_argument("start-maximized")
 options.add_argument('disable-infobars')
 options.add_argument("user-data-dir=selenium")
 prefs = {"profile.managed_default_content_settings.images": 2}
 options.add_experimental_option("prefs", prefs)
-
-
 driver = webdriver.Chrome(chrome_options=options)
 
+# Retreive Tkinter Variables
+class Application(tkinter.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.hi_there = tkinter.Button(self)
+        self.hi_there["text"] = "Run Program"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there["fg"] = "green"
+        self.hi_there.pack(side="bottom")
+
+        self.URL_label = tkinter.Label(self, text="Enter Website URL to SNKR shoe")
+        self.URL_label.pack()
+
+        self.URL = tkinter.Text(self, height=1, width=50, padx=1, pady=1)
+        self.URL.pack()
+
+        self.cc_label = tkinter.Label(self, text="Enter 3 digit code on back of CC")
+        self.cc_label.pack()
+
+        self.cc = tkinter.Text(self, height=1, width=10, padx=1, pady=1)
+        self.cc.pack()
+
+        self.quit = tkinter.Button(self, text="QUIT", fg="red", command=self.master.destroy)
+        self.quit.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
+
+#root = tkinter.Tk()
+#app = Application(master=root)
+#app.mainloop()
+
+# Constants
 CART = False
 SHOE_AMOUNT = 1
-SIZE=0
+SIZE="M 11 / W 12.5"
 retries = 0
 
 while True:
-    driver.get("https://www.nike.com/launch/t/zoom-type-hyper-pink")
+    #driver.get("https://www.nike.com/launch/t/zoom-type-hyper-pink")
+    #driver.get("https://www.nike.com/launch/t/air-jordan-1-baroque-brown-racer-pink")
+    driver.get("https://www.nike.com/launch/t/air-vapormax-2020-flyknit-smoke-grey")
     if CART == False:
         
         print("Selecting Size")
         size_path = '//*[@id="root"]/div/div/div[1]/div/div[3]/div[2]/div/section/div[2]/aside/div/div[2]/div/ul/li[13]/button'
-        size = 'M 11 / W 12.5'
-        size_dynamic_path = "//button[contains(text(),'" + size + "')]"
+        size_dynamic_path = "//button[contains(text(),'" + SIZE + "')]"
         print(size_dynamic_path)
 
         try:
-            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, size_dynamic_path))).click()
+            WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, size_dynamic_path))).click()
         except:
             retries = retries + 1
-            print("retries: ")
-            print(retries)
+            print("retries: %s ", retries)
+            #print(retries)
             continue
 
         # Buy
         print("Clicking buy button")
         buy_path = '//*[@id="root"]/div/div/div[1]/div/div[3]/div[2]/div/section/div[2]/aside/div/div[2]/div/button'
-        buy_dynamic_path = "//button[contains(text(),'" + 'BUY $' + "')]"
+        buy_dynamic_path = "//button[contains(text(),'" + 'BUY $150' + "')]"
         try:
-            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, buy_path))).click()
+            WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.XPATH, buy_path))).click()
         except:
             print("failed to click Buy Button")
 
@@ -56,7 +95,7 @@ while True:
 
     # Security Code
     print("entering sc code")
-    sc = '981'
+    sc = '565'
     sc_path = '//*[@id="cvNumber"]'
     WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, sc_path))).send_keys(sc)
 
